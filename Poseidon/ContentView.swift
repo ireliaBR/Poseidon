c37293c3d5e9599eb3e89f31c2b693fc34be1205
irelia <672575302@qq.com>
2024-01-23 16:01:20 +0800

    init

---
+++ Poseidon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poseidon
+//
+//  Created by fdd on 2024/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poseidon/PoseidonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseidonApp.swift
+//  Poseidon
+//
+//  Created by fdd on 2024/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PoseidonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
